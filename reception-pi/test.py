b2878275aca5d1e045fdbaf531bc724ca7453327
Boxuan Lu <547181293@qq.com>
2019-05-17 18:14:20 +1000

    create basic unittest for rp

---
+++ reception-pi/test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import unittest
+import Client
+
+
+class RPTest(unittest.TestCase):
+    def test_db_connection(self):
+        config_file = '../config.json'
+        config = Client.Config(config_file)
+        db = Client.Userdb(config)
+        self.assertIsNotNone(db)
+
+    def test_login(self):
+        config_file = '../config.json'
+        config = Client.Config(config_file)
+        db = Client.Userdb(config)
+
+        provided_pwd = "admin123"
+        hashed_pwd = db.hash_password(provided_pwd)
+        self.assertTrue(db.verify_password(hashed_pwd, provided_pwd))
+
+    def test_register(self):
+        config_file = '../config.json'
+        config = Client.Config(config_file)
+        db = Client.Userdb(config)
+        
+        connection = db._Userdb__conn
+        cursor = connection.cursor()
+
+        user_count = cursor.execute("""SELECT * FROM rpuser""")
+        print("\n" + str(user_count))
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
