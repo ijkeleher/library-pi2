a6cda292da0a2e30d372be1936a5113f923ba0a4
rmit-s3491161-christopher-heuvel <s3491161@student.rmit.edu.au>
2019-05-21 23:51:49 +1000

    face detect

---
+++ Reception Pi/Client.py
@@ -0,0 +1,300 @@
+#!/usr/bin/python3
+
+import sys
+import MySQLdb
+import json
+import hashlib, binascii, os
+import getpass
+import re
+import socket
+
+
+class Menu:
+
+
+    def displaymenu(self):
+        print("""
+        1.Login
+        2.Register new user
+        3.Exit
+        """)
+
+    def getselection(self):
+        while True:
+            self.displaymenu()
+
+            try:
+                selection = int(input("Enter Selection: "))
+
+                if 4 > selection > 0:
+                    return selection
+
+            except ValueError:
+                print("Error: enter valid selection")
+                continue
+
+    def login_option(self):
+        """
+        Return:
+            True if user choose to login with email
+            Flase if user choose to login with username
+            None if user don't want to login
+        """
+
+        print("""
+        1. Login with email
+        2. Login with username
+        3. Back to previous menu
+        """)
+
+        try:
+            selection = int(input("Enter login option: "))
+            if selection == 1:
+                return True
+            elif selection == 2:
+                return False
+            elif selection == 3:
+                return None
+        except ValueError:
+            print("Invalid option!")
+
+    def get_login_detail(self):
+        """
+        Return
+            username or email that user need to login with
+        """
+        detail = str(input("Please enter your email addresss: "))
+        return detail
+
+
+class Userdb:
+
+    def __init__(self, config):
+
+        self.__conn = MySQLdb.connect(config.gethostname(), config.getdbuser(), config.getdbpass(), config.getdbname())
+        self.email_addr = re.compile('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$')
+
+    def createuser(self):
+
+        inputvalid = False
+
+        chars = re.compile('^[a-zA-Z]+$')
+        charsnums = re.compile('^[a-zA-Z0-9]+$')
+        emailaddr = self.email_addr
+
+        while not inputvalid:
+
+            username = input("Enter Username: ")
+
+            if re.fullmatch(charsnums, username) is None:
+                print("Error: invalid username")
+                continue
+
+            passwordraw = getpass.getpass("Enter Password: ")
+            confirm_password = getpass.getpass("Confirm Password: ")
+
+            if passwordraw != confirm_password:
+                print("Password doesn't match!")
+                continue
+
+            passwordhashed = self.hash_password(passwordraw)
+
+
+            firstname = input("Enter First name: ")
+            lastname = input("Enter Last name: ")
+
+            if re.fullmatch(chars, firstname) is None or re.fullmatch(chars, lastname) is None:
+                print("Error: invalid first or last name ")
+                continue
+
+
+            email = input("Enter Email: ")
+
+            if re.fullmatch(emailaddr, email) is None:
+                print("Error: invalid email ")
+                continue
+
+            inputvalid = True
+
+        cursor = self.__conn.cursor()
+
+        params = (username, passwordhashed, firstname, lastname, email)
+
+        cursor.execute("INSERT INTO rpuser(username, password, firstname, lastname, email) VALUES (%s,%s,%s,%s,%s)", params)
+
+        self.__conn.commit()
+
+    def loginvalid(self):
+        pass
+
+    def hash_password(self, password):
+        """Reference: https://www.vitoshacademy.com/hashing-passwords-in-python/"""
+        salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
+        pwdhash = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'), salt, 100000)
+        pwdhash = binascii.hexlify(pwdhash)
+        return (salt + pwdhash).decode('ascii')
+
+    def verify_password(self, stored_password, provided_password):
+        """Reference: https://www.vitoshacademy.com/hashing-passwords-in-python/"""
+        salt = stored_password[:64]
+        stored_password = stored_password[64:]
+        pwdhash = hashlib.pbkdf2_hmac('sha512', provided_password.encode('utf-8'), salt.encode('ascii'), 100000)
+        pwdhash = binascii.hexlify(pwdhash).decode('ascii')
+        return pwdhash == stored_password
+
+    def login(self, detail, email_login):
+        """
+        Param:
+            detail: login detail, either username or email
+            email_login: True if user choose to login with email, False if user choose to login with username
+        Return:
+            True if login is valid
+            False otherwise
+        """
+        if email_login:
+            if re.fullmatch(self.email_addr, detail) is None:
+                print("Invalid email!")
+            else:
+                cursor = self.__conn.cursor()
+                cursor.execute("SELECT * FROM rpuser WHERE email = %s", [detail])
+                data = cursor.fetchone()
+                if data is not None:
+                    print(data[1])
+                    stored_password = data[1]
+
+                    password = getpass.getpass("Please enter your password: ")
+                    return self.verify_password(stored_password, password)
+        else:
+            cursor = self.__conn.cursor()
+            cursor.execute("SELECT * FROM rpuser WHERE username = %s", [detail])
+            data = cursor.fetchone()
+            if data is not None:
+                print(data[1])
+                stored_password = data[1]
+
+                password = getpass.getpass("Please enter your password: ")
+                return self.verify_password(stored_password, password)
+
+
+class Config:
+
+    def __init__(self, filename):
+        try:
+            with open(filename) as data:
+                self.__conf = json.load(data)
+        except EnvironmentError:
+            print("Can't open "+filename)
+
+    def getdbuser(self):
+        return self.__conf['dbuser']
+
+    def getdbpass(self):
+        return self.__conf['dbpass']
+
+    def getdbname(self):
+        return self.__conf['dbname']
+
+    def gethostname(self):
+        return self.__conf['hostname']
+
+
+class SocketSession:
+    
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+    
+
+    def Connect(self, user):
+        print("Establishing connection to remote host @ " + self.host+":"+str(self.port))
+      
+        self.user = user
+
+        # Connect
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        self.sock.connect((self.host, self.port))
+
+        # Start by sending user info
+        self.sock.sendall(bytes(user, 'UTF-8')) 
+        # Get back the main menu
+        menu = self.sock.recv(4096)
+
+        return menu
+
+    def ConsoleSession(self):
+       
+        while True:
+           
+            # Get some user input
+            inp = input("Please enter your response: ")
+            
+            # Shoot it off to the server
+            self.sock.sendall(bytes(inp, 'UTF-8'))
+           
+            # Get the response
+            response = self.sock.recv(4096)
+            print(response)
+
+            # if response is 'TERMINATE':
+            #   break
+
+
+
+
+class Main:
+
+    def __init__(self):
+        self.host = '127.0.0.1'
+        self.port = 6969
+
+
+    def RemoteMenu(self, user):
+        print("Logged in succesfully!")
+
+        session = SocketSession(self.host, self.port)
+
+        main_menu = session.Connect(user)
+
+        print(main_menu)
+
+        session.ConsoleSession()
+
+
+
+
+    def main(self):
+
+        menu = Menu()
+        configfile = '../config.json' # set path to config.jsons
+        config = Config(configfile)
+        db = Userdb(config)
+
+        while True:
+            selection = menu.getselection()
+
+            if selection == 1:
+                login_with_email = menu.login_option()
+                if login_with_email == None:
+                    continue
+                elif login_with_email:
+                    email = menu.get_login_detail()
+                    valid_login = db.login(email, True)
+                    if valid_login:
+                        print("Login Successfully")
+                    else:
+                        print("Email or password is not correct!")
+                elif not login_with_email:
+                    username = menu.get_login_detail()
+                    valid_login = db.login(username, False)
+                    if valid_login:
+                        print("Login Successfully")
+                    else:
+                        print("Username or password is not correct!")
+
+            elif selection == 2:
+                db.createuser()
+            else:
+                sys.exit(0)
+
+if __name__ == "__main__":
+    Main().main()
