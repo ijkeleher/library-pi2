6952b829218d64795349832a64eabcb447ebf377
rmit-s3491161-christopher-heuvel <s3491161@student.rmit.edu.au>
2019-05-22 12:44:44 +1000

    update

--- reception-pi/Client.py
+++ reception-pi/Client.py
@@ -7,8 +7,15 @@ import hashlib, binascii, os
 import getpass
 import re
 import socket
-from facialrecognition import recognise
-from QRscan import qrscan
+from imutils.video import VideoStream
+from pyzbar import pyzbar
+import datetime
+import imutils
+import time
+import cv2
+import face_recognition
+import argparse
+import pickle
 
 class Menu:
 
@@ -246,6 +253,112 @@ class SocketSession:
             #   break
 
 
+class QRscan:
+
+    def scan(self):
+
+        # initialize the video stream and allow the camera sensor to warm up
+        print("[INFO] starting video stream...")
+        vs = VideoStream(src=0).start()
+        time.sleep(2.0)
+
+        found = set()
+
+        # loop over the frames from the video stream
+        while True:
+            # grab the frame from the threaded video stream and resize it to
+            # have a maximum width of 400 pixels
+            frame = vs.read()
+            frame = imutils.resize(frame, width=400)
+
+            # find the barcodes in the frame and decode each of the barcodes
+            barcodes = pyzbar.decode(frame)
+
+            # loop over the detected barcodes
+            for barcode in barcodes:
+                # the barcode data is a bytes object so we convert it to a string
+                barcodeData = barcode.data.decode("utf-8")
+                barcodeType = barcode.type
+                return barcodeData
+
+
+class Recognise:
+
+    def getuser(self):
+
+        # construct the argument parser and parse the arguments
+        ap = argparse.ArgumentParser()
+        ap.add_argument("-e", "--encodings", default="encodings.pickle",
+        help="path to serialized db of facial encodings")
+        ap.add_argument("-r", "--resolution", type=int, default=240,
+            help="Resolution of the video feed")
+        ap.add_argument("-d", "--detection-method", type=str, default="hog",
+            help="face detection model to use: either `hog` or `cnn`")
+        args = vars(ap.parse_args())
+
+        # load the known faces and embeddings
+        print("[INFO] loading encodings...")
+        data = pickle.loads(open(args["encodings"], "rb").read())
+
+        # initialize the video stream and then allow the camera sensor to warm up
+        print("[INFO] starting video stream...")
+        vs = VideoStream(src = 0).start()
+        time.sleep(2.0)
+
+        # loop over frames from the video file stream
+        while True:
+            # grab the frame from the threaded video stream
+            frame = vs.read()
+
+            # convert the input frame from BGR to RGB then resize it to have
+            # a width of 750px (to speedup processing)
+            rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            rgb = imutils.resize(frame, width = args["resolution"])
+
+            # detect the (x, y)-coordinates of the bounding boxes
+            # corresponding to each face in the input frame, then compute
+            # the facial embeddings for each face
+            boxes = face_recognition.face_locations(rgb, model = args["detection_method"])
+            encodings = face_recognition.face_encodings(rgb, boxes)
+            names = []
+
+            # loop over the facial embeddings
+            for encoding in encodings:
+                # attempt to match each face in the input image to our known
+                # encodings
+                matches = face_recognition.compare_faces(data["encodings"], encoding)
+                name = "Unknown"
+
+                # check to see if we have found a match
+                if True in matches:
+                    # find the indexes of all matched faces then initialize a
+                    # dictionary to count the total number of times each face
+                    # was matched
+                    matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+                    counts = {}
+
+                    # loop over the matched indexes and maintain a count for
+                    # each recognized face face
+                    for i in matchedIdxs:
+                        name = data["names"][i]
+                        counts[name] = counts.get(name, 0) + 1
+
+                    # determine the recognized face with the largest number
+                    # of votes (note: in the event of an unlikely tie Python
+                    # will select first entry in the dictionary)
+                    name = max(counts, key = counts.get)
+
+                # update the list of names
+                names.append(name)
+
+           # loop over the recognized faces
+            for name in names:
+                # print to console, identified person
+                print("Person found: {}".format(name))
+                # Set a flag to sleep the cam for fixed time
+                vs.stop()
+                return name
+
 
 
 class Main:
@@ -272,7 +385,7 @@ class Main:
     def main(self):
 
         menu = Menu()
-        configfile = '../config.json' # set path to config.jsons
+        configfile = 'config.json' # set path to config.jsons
         config = Config(configfile)
         db = Userdb(config)
 
@@ -304,10 +417,12 @@ class Main:
             elif selection == 2:
                 db.createuser()
             elif selection == 3:
+                recognise = Recognise()
                 user = recognise.getuser()
                 self.RemoteMenu(user)
 
             elif selection == 4:
+                qrscan = QRscan()
                 book = qrscan.scan()
                 print("Book returned: " + book)
 
