00cb152b14aefe444f8c27c0248a3bed5317b030
BoxHezi <547181293@qq.com>
2019-05-26 14:53:01 +1000

    move qrscan to separate file

--- reception-pi/Client.py
+++ reception-pi/Client.py
@@ -10,7 +10,7 @@ import socket
 from shutil import copy2
 from facialrecognition.recognise import Recognise
 from speech import Speech2Text
-
+from qrscan import QRscan
 from imutils.video import VideoStream
 from pyzbar import pyzbar
 import datetime
@@ -405,43 +405,6 @@ class SocketSession:
                 break
             
             print(response)
-
-
-class QRscan:
-    """
-    QR Code scanner
-    """
-
-    def scan(self):
-        """
-        scan qr code to get information
-        Return:
-            barcodeData: a bytes object
-        """
-
-        # initialize the video stream and allow the camera sensor to warm up
-        print("[INFO] starting video stream...")
-        vs = VideoStream(src=0).start()
-        time.sleep(2.0)
-
-        found = set()
-
-        # loop over the frames from the video stream
-        while True:
-            # grab the frame from the threaded video stream and resize it to
-            # have a maximum width of 400 pixels
-            frame = vs.read()
-            frame = imutils.resize(frame, width=400)
-
-            # find the barcodes in the frame and decode each of the barcodes
-            barcodes = pyzbar.decode(frame)
-
-            # loop over the detected barcodes
-            for barcode in barcodes:
-                # the barcode data is a bytes object so we convert it to a string
-                barcodeData = barcode.data.decode("utf-8")
-                barcodeType = barcode.type
-                return barcodeData
 
 
 class Main:
