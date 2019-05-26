3ec8c1eaad3d878d0627dfbbb59cf293aabf746d
Isaac Keleher <isaac.keleher@gmail.com>
2019-05-26 13:25:34 +1000

    Added login and logout func

--- master-pi/flask_main.py
+++ master-pi/flask_main.py
@@ -1,14 +1,27 @@
-# pip3 install flask flask_sqlalchemy flask_marshmallow marshmallow-sqlalchemy
-# python3 flask_main.py
-from flask import Flask, request, jsonify, render_template
+'''
+@author Inci Keleher
+
+This file contains the application logic for the admin website
+    - Database init
+    - Login function
+    - Logout function
+
+Site endpoints are located in flask_site.py
+API endpoints located in flask_api.py
+
+Code modified from Tutorial code
+'''
+from flask import Flask, request, jsonify, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os, requests, json
 from flask_api import api, db
 from flask_site import site
+import flask_login
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
+app.secret_key = 'super secret string'  # Change this!
 
 # Update HOST and PASSWORD appropriately.
 HOST = "35.189.9.24"
@@ -20,6 +33,70 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://{}:{}@{}/{}".format(USER, PASSW
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 db.init_app(app)
+
+#setup flask-login
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+# Our mock database.
+users = {'foo@bar.tld': {'password': 'secret'}}
+
+class User(flask_login.UserMixin):
+    pass
+
+@login_manager.user_loader
+def user_loader(email):
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+
+    # DO NOT ever store passwords in plaintext and always compare password
+    # hashes using constant-time comparison!
+    user.is_authenticated = request.form['password'] == users[email]['password']
+
+    return user
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return '''
+               <form action='login' method='POST'>
+                <input type='text' name='email' id='email' placeholder='email'/>
+                <input type='password' name='password' id='password' placeholder='password'/>
+                <input type='submit' name='submit'/>
+               </form>
+               '''
+
+    email = request.form['email']
+    if request.form['password'] == users[email]['password']:
+        user = User()
+        user.id = email
+        flask_login.login_user(user)
+        return redirect(url_for('site.index'))
+
+    return 'Bad login'
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return 'Logged out'
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return 'Unauthorized'
 
 app.register_blueprint(api)
 app.register_blueprint(site)
